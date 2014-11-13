cab0a0fb3352a3e003e387239df5159178af982a
marguerite <i@marguerite.su>
2014-11-14 02:29:20 +0800

    release 0.0.2

--- BiliGui.rb
+++ BiliGui.rb
@@ -9,6 +9,7 @@ class QtApp < Qt::Widget
 
 	slots 'bilidan()'
 	slots 'clear()'
+	slots 'bilidanChoose()'
 
 	def initialize
 		super
@@ -40,21 +41,27 @@ class QtApp < Qt::Widget
 	def init_ui
 		grid = Qt::GridLayout.new self
 
-		biliUrlLabel = Qt::Label.new "Please paste Bilibili URL below", self
 		bilidanPathLabel = Qt::Label.new "Please enter your bilidan's path:", self
+		@bilidanPath = Qt::LineEdit.new self
+		bilidanButton = Qt::PushButton.new 'Choose', self
+		biliUrlLabel = Qt::Label.new "Please paste Bilibili URL below", self
 		@urlArea = Qt::TextEdit.new self
-		@bilidanPath = Qt::LineEdit.new self	
-		okButton = Qt::PushButton.new 'Fire!', self
+		@messageLabel = Qt::Label.new "", self
+		@messageLabel.setStyleSheet("color: #ff0000;")	
+		okButton = Qt::PushButton.new 'Watch', self
 		clearButton = Qt::PushButton.new 'Clear', self
 
 		grid.addWidget bilidanPathLabel, 0, 0, 1, 1
 		grid.addWidget @bilidanPath, 0, 1, 1, 2
-		grid.addWidget biliUrlLabel, 1, 0, 1, 3
-		grid.addWidget @urlArea, 2, 0, 1, 3
-		grid.addWidget okButton, 3, 1, 1, 1
-		grid.addWidget clearButton, 3, 2, 1, 1
-		grid.setColumnStretch 3, 0
+		grid.addWidget bilidanButton, 0, 3, 1, 1
+		grid.addWidget biliUrlLabel, 1, 0, 1, 4
+		grid.addWidget @urlArea, 2, 0, 1, 4
+		grid.addWidget @messageLabel, 3, 0, 1, 1
+		grid.addWidget okButton, 3, 2, 1, 1
+		grid.addWidget clearButton, 3, 3, 1, 1
+		grid.setColumnStretch 1, 2
 
+		connect bilidanButton, SIGNAL('clicked()'), self, SLOT('bilidanChoose()')
 		connect okButton, SIGNAL('clicked()'), self, SLOT('bilidan()')
 		connect clearButton, SIGNAL('clicked()'), self, SLOT('clear()')
 	end
@@ -65,17 +72,28 @@ class QtApp < Qt::Widget
 		if urlText != "" then
 			if pathText != "" then
 				command = "#{pathText}/bilidan.py #{urlText}"
-			else
+				exec command
+			elsif File.exists?('./bilidan.py') then
 				command = "./bilidan.py #{urlText}"
+				exec command
+			else
+				error = "[ERR] you need to choose bilidan!"
+				@messageLabel.setText(error)
 			end
-			exec command
 		else
-			puts "[ERR] you have to paste an URL!"
+			error = "[ERR] you have to paste an URL!"
+			@messageLabel.setText(error)
 		end
 	end
 
 	def clear
 		@urlArea.clear()
+	end
+
+	def bilidanChoose
+		userHome = `echo $HOME`.gsub(/\n/,"")
+		bilidanBin = Qt::FileDialog.getOpenFileName(self, "Please choose your bilidan.py", "#{userHome}", "Python files (*.py)")
+		@bilidanPath.setText(bilidanBin)
 	end
 
 end
--- README.md
+++ README.md
@@ -1,21 +1,17 @@
-== BiliGui ==
+## BiliGui
 
 Bilibili GUI for [BiliDan](https://github.com/m13253/BiliDan)
 
-=== Installation ===
+### Installation
 
-You need ruby-qt. In openSUSE
+You need ruby-qt. In openSUSE `sudo zypper in ruby-qt` will do the work
 
-    zypper in ruby-qt
+#### Optional
 
-will do the work
-
-Then put BiliGui.rb to the same directory as your BiliDan goes.
-
-And place BiliGui.desktop anywhere that is easy to you.
+Place BiliGui.desktop anywhere that is easy to you.
 
 Remeber to edit the absolute paths in BiliGui.desktop
 
-=== License ===
+### License
 
 MIT 
